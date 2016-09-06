cd90002acd524b8e0404b010cfc68742932182e6
Calvin McClure <session1@START-Coding-MBP-16.local>
2016-09-06 14:47:13 -0700

    created skeleton of project

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
-/**
- * Created by session1 on 9/6/16.
- */
+canvas = document.getElementById("canvas");
+context = canvas.getContext("2d");
+canvas.style.width = window.innerWidth - 32 + "px";
+canvas.style.height = window.innerHeight - 32 + "px";
+
+function update (){
+
+}
+
+function render (){
+    context.clearRect(0,0,800,480);
+
+    requestAnimationFrame(render);
+}
+
+function init (){
+
+
+    setInterval(update, 1000 / 60);
+    requestAnimationFrame(render);
+}
+
+init();
