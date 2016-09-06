efa988e0fd65404bf7321fe2a3dcc0c5b0b0469f
Calvin McClure <session1@START-Coding-MBP-16.local>
2016-09-06 14:53:23 -0700

    commented javascript file out

--- scripts.js
+++ scripts.js
@@ -1,18 +1,24 @@
+//creates context element that connects to html and defines
 canvas = document.getElementById("canvas");
 context = canvas.getContext("2d");
+
+//sets canvas dimensions
 canvas.style.width = window.innerWidth - 32 + "px";
 canvas.style.height = window.innerHeight - 32 + "px";
 
+//updates values and code
 function update (){
 
 }
 
+//draws code
 function render (){
     context.clearRect(0,0,800,480);
 
     requestAnimationFrame(render);
 }
 
+//runs once, sets up game
 function init (){
 
 
