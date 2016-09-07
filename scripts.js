22741483bb43c097eb452a41b75fbb7d89e05f50
Calvin_Home <calvin.mcclure@west-mec.org>
2016-09-06 21:11:53 -0700

    Created a Player constructor and tested it with a little snowman. Also added a pics directory.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,59 @@
-//creates context element that connects to html and defines
-canvas = document.getElementById("canvas");
-context = canvas.getContext("2d");
+// creates context element that connects to html and defines
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
 
-//sets canvas dimensions
-canvas.style.width = window.innerWidth - 32 + "px";
-canvas.style.height = window.innerHeight - 32 + "px";
+// Implicitly declaring variables
+var snowman;
 
-//updates values and code
+// sets canvas dimensions
+// //canvas.style.width = window.innerWidth - 32 + "px";
+// //canvas.style.height = window.innerHeight - 32 + "px";
+
+// updates values and code
 function update (){
 
 }
 
-//draws code
+// draws code
 function render (){
+    //clears the canvas
     context.clearRect(0,0,800,480);
+    snowman.render();
 
     requestAnimationFrame(render);
 }
 
-//runs once, sets up game
+// runs once, sets up game
 function init (){
 
+    // creates a variable that holds all the img
+    // information for the snowman sprite
+    var snowmanImg = new Image();
+    snowmanImg.src = "pics/snowman.png";
+
+    // uses Player constructor to make new snowman
+    snowman = new Player(100, canvas.height - 150, 200, 200, snowmanImg);
+
+    // creates a refresh rate for the canvas
     setInterval(update, 1000 / 60);
     requestAnimationFrame(render);
 }
 
+// creates a reusable constructor function
+function Player (x, y, width, height, sprite){
+    // uses parameters to set css values for the Player
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.sprite = sprite;
+
+    // function that creates the img on canvas
+    this.render = function () {
+        context.drawImage(this.sprite, this.x, this.y, this.width, this.height);
+    }
+}
+
+// runs the function that starts
+// the rest of the processes
 init();
